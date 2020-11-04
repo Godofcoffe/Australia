35152fd1c43d09866c17245ca429e54553fc9cc8
Godofcoffe <robotnyte@gmail.com>
2020-11-04 16:10:19 -0300

    Update tabuada.py

--- australia.py/tabuada.py
+++ australia.py/tabuada.py
@@ -1,16 +1,13 @@
 """
 Use um número negativo para para o loop.
 """
-c = 1
+
 while True:
     print('-' * 60)
     n = int(input('Digite um número para eu te dizer a tabuada: '))
     print('-' * 60)
-    c = 1
-
     if n < 0:
         break
-    while c <= 10:
-        print(f'{n} X {c:>2} = {n * c:>2}')
-        c += 1
+    for c in range(1, 11):
+        print(f'{n} X {c:>2} = {n*c:>2}')
 print('finish')
