b4f6bf7b7974373d37057bad437facbf74ace219
Godofcoffe <robotnyte@gmail.com>
2020-11-10 12:34:19 -0300

    Update tabulaçao de preços.py

--- australia.py/tabulaçao de preços.py
+++ australia.py/tabulaçao de preços.py
@@ -1,8 +1,10 @@
-produtos = ('Pão', 0.30, 'Leite', 4.00, 'Macarrão', 3, 'Boneca Inflavel', 10, 'Café Genérico', 50)
-print('-'*40)
+produtos = ('Pão', 0.30, 'Leite', 4.00, 'Macarrão', 3, 'Boneca Inflavel', 10, 'Café Genérico', 50,
+            'Canhão de Plasma', 10)  # valores por unidade.
+
+print('-' * 40)
 print(f'{"TABELA DE PREÇOS":^40}')
-print('-'*40)
+print('-' * 40)
 tam = len(produtos)
 for c in range(0, tam, 2):
-    print(f'{produtos[c]:.<30}R${float(produtos[c+1]):>8.2f}')
-print('-'*40)
+    print(f'{produtos[c]:.<30}R${float(produtos[c + 1]):>8.2f}')
+print('-' * 40)
