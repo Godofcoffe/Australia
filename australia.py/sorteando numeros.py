2ede85214d413a1d5f4d84068fa7ff1b96c39894
Godofcoffe <robotnyte@gmail.com>
2020-11-10 12:16:40 -0300

    Update sorteando numeros.py

--- australia.py/sorteando numeros.py
+++ australia.py/sorteando numeros.py
@@ -1,13 +1,11 @@
 from random import randint
 
-n1 = str(randint(1, 5))
-n2 = str(randint(1, 5))
-n3 = str(randint(1, 5))
-n4 = str(randint(1, 5))
-n5 = str(randint(1, 5))
+n1 = randint(1, 5)
+n2 = randint(1, 5)
+n3 = randint(1, 5)
+n4 = randint(1, 5)
+n5 = randint(1, 5)
 sorteados = (n1, n2, n3, n4, n5)
-print(sorteados)
-menor = sorted(sorteados)[0]
-maior = sorted(sorteados)[-1]
-print(f'O maior é {maior}')
-print(f'E o menor é {menor}')
+print(f'Foram sortados {sorteados}')
+print(f'O maior é {sorted(sorteados)[-1]}')
+print(f'E o menor é {sorted(sorteados)[0]}')
