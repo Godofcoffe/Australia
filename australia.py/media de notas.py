e6ef448f7151a5d325d34d694897c91d42675fa5
Godofcoffe <robotnyte@gmail.com>
2020-11-11 21:28:20 -0300

    Update media de notas.py

--- australia.py/media de notas.py
+++ australia.py/media de notas.py
@@ -1,7 +1,9 @@
 nota = float(input('Primeira nota:'))
 nota2 = float(input('Segunda nota:'))
-media = (nota + nota2) / 2
-print(f'Com as notas {nota} e {nota2} e com a media {media}')
+nota3 = float(input('Terceira nota: '))
+nota4 = float(input('Quarta nota: '))
+media = (nota + nota2 + nota3 + nota4) / 4
+print(f'Com as notas {nota}, {nota2}, {nota3} e {nota4} e com a media {media}')
 print('O aluno esta:')
 if media < 5:
     print('REPROVADO!')
