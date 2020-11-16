ef3b2d5e707f538e55726f8693389688243196a0
Godofcoffe <robotnyte@gmail.com>
2020-11-16 17:53:21 -0300

    Create ex079.py

---
+++ australia.py/ex079.py
@@ -0,0 +1,22 @@
+"""Exercício Python 079: Crie um programa onde o usuário possa digitar vários valores numéricos e cadastre-os em uma
+lista. Caso o número já exista lá dentro, ele não será adicionado. No final, serão exibidos todos os valores únicos
+digitados, em ordem crescente. """
+nuns = list()
+while True:
+    entrada = str(input('Número: '))
+    if entrada in nuns:
+        print(f'Número ja existente...')
+    else:
+        nuns.append(entrada)
+    opc = input('Quer continuar? [s/n]: ').lower().strip()[0]
+    if opc == 'n':
+        break
+    elif opc == 's':
+        pass
+    else:
+        print('opçao invalida')
+print('Voce digitou esse numeros: ')
+crescente = nuns[:]
+crescente.sort()
+for n in crescente:
+    print(n, end=' ')
