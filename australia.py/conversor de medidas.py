6de86073287ed4c0f191bdf5b43f6cd53a176f15
Godofcoffe <robotnyte@gmail.com>
2020-11-22 14:35:40 -0300

    Update conversor de medidas.py

--- australia.py/conversor de medidas.py
+++ australia.py/conversor de medidas.py
@@ -1,7 +1,6 @@
 medida = int(input('digite um numero em metros:'))
 
-print(f' centimetros :{medida*100}')
-print(f' milimetros :{medida*1000}')
-print(f'decametros :{medida/10}')
-# print(f'{medida/100}')
-print(f'kilometros :{medida/1000}')
+print(f'> Centimetros :{medida*100}')
+print(f'> Milimetros :{medida*1000}')
+print(f'> Decametros :{medida/10}')
+print(f'> Kilometros :{medida/1000}')
